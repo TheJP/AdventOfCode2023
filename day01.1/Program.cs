6f637ab37e9a29f5544c1345277ec0540601bf05
TheJP <jp_55555@hotmail.com>
2023-12-01 06:45:23 +0100

    Solved day 1 task 1

---
+++ day01.1/Program.cs
@@ -0,0 +1,8 @@
+﻿var input = File.ReadLines(Environment.GetCommandLineArgs()[1]);
+int sum = 0;
+foreach (var line in input)
+{
+    sum += int.Parse($"{line.First(char.IsDigit)}{line.Last(char.IsDigit)}");
+}
+
+Console.WriteLine(sum);
