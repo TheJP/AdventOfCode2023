2d6ffb7659880fecab302ad5c8a47efbfad168cf
TheJP <jp_55555@hotmail.com>
2023-12-04 18:09:08 +0100

    Solved day 4 task 1

---
+++ day04.1/Program.cs
@@ -0,0 +1,16 @@
+﻿var input = File.ReadLines(Environment.GetCommandLineArgs()[1]);
+int sum = 0;
+foreach (var line in input)
+{
+    var numbers = line.Split(":")[1].Split("|").Select(part => part
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse))
+        .ToArray();
+
+    var winning = numbers[0].ToHashSet();
+    var wins = numbers[1].Count(winning.Contains);
+
+    if (wins > 0) sum += 1 << (wins - 1);
+}
+
+Console.WriteLine(sum);
