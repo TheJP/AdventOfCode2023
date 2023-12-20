b5f7c9a5270ae5bd1ea5ccde9d5fcdb97b4193cf
TheJP <jp_55555@hotmail.com>
2023-12-20 21:38:09 +0100

    Solved day 6 task 1

---
+++ day06.1/Program.cs
@@ -0,0 +1,21 @@
+﻿var input = File.ReadAllLines(Environment.GetCommandLineArgs()[1]);
+var numbers = input.Select(line => line.Split(':')[1]
+    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray()).ToArray();
+var times = numbers[0];
+var distances = numbers[1];
+
+int result = 1;
+
+for (int i = 0; i < times.Length; ++i)
+{
+    int wins = 0;
+    for (int speed = 1; speed < times[i]; ++speed)
+    {
+        if ((times[i] - speed) * speed > distances[i]) ++wins;
+    }
+    result *= wins;
+}
+
+Console.WriteLine(result);
