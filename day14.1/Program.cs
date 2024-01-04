456e231a51eb8cbdadeffc66f2039ca51f941e44
TheJP <jp_55555@hotmail.com>
2024-01-04 17:35:40 +0100

    Solved day 14 task 1

---
+++ day14.1/Program.cs
@@ -0,0 +1,21 @@
+﻿var input = File.ReadAllLines(Environment.GetCommandLineArgs()[1]);
+
+int sum = 0;
+
+for (int x = 0; x < input[0].Length; ++x)
+{
+    int rocks = input.Length;
+    for (int y = 0; y < input.Length; ++y)
+    {
+        if (input[y][x] == '.') continue;
+        if (input[y][x] == '#')
+        {
+            rocks = input.Length - y - 1;
+            continue;
+        }
+        sum += rocks;
+        --rocks;
+    }
+}
+
+Console.WriteLine(sum);
