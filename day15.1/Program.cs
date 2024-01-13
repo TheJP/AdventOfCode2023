fed0d1311b486acc07995de974d2ddcce8120d1a
TheJP <jp_55555@hotmail.com>
2024-01-13 12:48:52 +0100

    Solved day 15 task 1

---
+++ day15.1/Program.cs
@@ -0,0 +1,18 @@
+﻿var input = File.ReadAllText(Environment.GetCommandLineArgs()[1]).Split(',');
+
+int sum = 0;
+
+foreach (var value in input)
+{
+    int hash = 0;
+    foreach (var c in value)
+    {
+        hash += c;
+        hash *= 17;
+        hash %= 256;
+    }
+
+    sum += hash;
+}
+
+Console.WriteLine(sum);
